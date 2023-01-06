e791c24ad77ff21668727dc7b4f7865251e72649
Yurii Rybachok <drummer2009@ukr.net>
2023-01-06 12:39:36 +0200

    array keys for sound

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>Hello, World!!</div>
+  <div>
+    <h1>Vue Drumkit</h1>
+    <div class="keys"></div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  data() {
+    return {
+      keys: [
+        {
+          name: 'boom',
+          audio: require("./assets/sounds/boom.wav"),
+          letter: "A"
+        },
+        {
+          name: 'clap',
+          audio: require("./assets/sounds/clap.wav"),
+          letter: "S"
+        },
+        {
+          name: 'hihat',
+          audio: require("./assets/sounds/hihat.wav"),
+          letter: "D"
+        },
+        {
+          name: 'kick',
+          audio: require("./assets/sounds/kick.wav"),
+          letter: "F"
+        },
+        {
+          name: 'openhat',
+          audio: require("./assets/sounds/openhat.wav"),
+          letter: "G"
+        },
+        {
+          name: 'ride',
+          audio: require("./assets/sounds/ride.wav"),
+          letter: "H"
+        },
+        {
+          name: 'snare',
+          audio: require("./assets/sounds/snare.wav"),
+          letter: "J"
+        },
+        {
+          name: 'tink',
+          audio: require("./assets/sounds/tink.wav"),
+          letter: "K"
+        },
+        {
+          name: 'tom',
+          audio: require("./assets/sounds/tom.wav"),
+          letter: "L"
+        },
+      ]
+    }
+  },
   components: {
   }
 }
@@ -19,5 +73,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.keys {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
